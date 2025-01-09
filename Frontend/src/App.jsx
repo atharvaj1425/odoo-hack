2ae779abaae05e8edc5ffb28f3741eee4ee31d3d
Atharva Jamdade <jamdadeatharva14@gmail.com>
2025-01-09 23:24:31 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='px-7 py-50 red-300'>Hello world</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
